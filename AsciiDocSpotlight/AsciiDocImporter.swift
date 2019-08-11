a566ecce8debd0f07707a3c43345929bda9a0274
Clyde Clements <clyde.clements@gmail.com>
2019-08-11 08:52:22 -0230

    Update to Swift 5 and make use of guards for error handling.

--- AsciiDocSpotlight/AsciiDocImporter.swift
+++ AsciiDocSpotlight/AsciiDocImporter.swift
@@ -13,87 +13,89 @@ import Foundation
                            attributes: NSMutableDictionary) -> Bool {
         NSLog("AsciiDocImporter import")
         let fileManager = FileManager()
-        if let data = fileManager.contents(atPath: pathToFile as String),
-            let content = String.init(data: data, encoding: .utf8) {
-            NSLog("AsciiDoc file read: \(pathToFile)")
-            attributes[kMDItemTextContent] = content
-            attributes[kMDItemKind] = "Plain Text Document"
-            let lines = content.components(separatedBy: "\n")
-            NSLog("AsciiDoc file number of lines: \(lines.count)")
-            if lines.count == 0 {
-                return true
-            }
-            var properties = [String: String]()
-            let firstLine = lines[0]
-            let titleLineRegex = try? NSRegularExpression(pattern: "^= (.*)$")
-            let range = NSRange(location: 0, length: firstLine.count)
-            let titleLineMatch = titleLineRegex!.matches(
-                in: firstLine, options: [], range: range
-            )
-            if titleLineMatch.count > 0 {
-                let title = titleLineRegex!.stringByReplacingMatches(
-                    in: firstLine, options: [], range: range, withTemplate: "$1"
-                )
-                if title.count > 0 {
-                    properties["doctitle"] = title
-                }
-            }
-            let fieldLineRegex = try? NSRegularExpression(pattern: "^:(\\w+): (.*)")
-            for i in 1..<lines.count {
-                let line = lines[i]
-                let allOfLine = NSRange(location: 0,
-                                        length: line.count)
-                let match = fieldLineRegex!.matches(in: line, options: [],
-                                                    range: allOfLine)
-                if match.count > 0 {
-                    let key = fieldLineRegex!.stringByReplacingMatches(
-                        in: line, options: [], range: allOfLine,
-                        withTemplate: "$1"
-                    )
-                    let value = fieldLineRegex!.stringByReplacingMatches(
-                        in: line, options: [], range: allOfLine,
-                        withTemplate: "$2"
-                    )
-                    properties[key] = value
-                } else {
-                    break
-                }
-            }
-            NSLog("AsciiDoc file attributes: \(properties)")
-            if let title = properties["doctitle"] {
-                attributes[kMDItemTitle] = title
-            }
-            if let author = properties["author"] {
-                attributes[kMDItemAuthors] = [author]
-            }
-            if let created = properties["created"] {
-                attributes[kMDItemContentCreationDate] = created
-            }
-            if let revdate = properties["revdate"] {
-                attributes[kMDItemContentModificationDate] = revdate
-            }
-            var keywords: [String] = []
-            if let keyword_prop = properties["keywords"] {
-                let terms = keyword_prop.components(separatedBy: ",")
-                for term in terms {
-                    let keyword = term.trimmingCharacters(in: .whitespaces)
-                    keywords.append(keyword)
-                }
-            }
-            if let category_prop = properties["categories"] {
-                let terms = category_prop.components(separatedBy: ",")
-                for term in terms {
-                    let category = "_" + term.trimmingCharacters(in: .whitespaces)
-                    keywords.append(category)
-                }
-            }
-            if keywords.count > 0 {
-                attributes[kMDItemKeywords] = keywords
-            }
-            return true
-        } else {
-            NSLog("Unable to read content of file \(pathToFile) to a string in UTF-8 encoding")
+        guard let data = fileManager.contents(atPath: pathToFile as String) else {
+            NSLog("Unable to read content of file \(pathToFile) to a string")
             return false
         }
+        guard let content = String.init(data: data, encoding: .utf8) else {
+            NSLog("Unable to parse content of file \(pathToFile) as a UTF-8 encoded string")
+            return false
+        }
+        NSLog("AsciiDoc file read: \(pathToFile)")
+        attributes[kMDItemTextContent!] = content
+        attributes[kMDItemKind!] = "Plain Text Document"
+        let lines = content.components(separatedBy: "\n")
+        NSLog("AsciiDoc file number of lines: \(lines.count)")
+        if lines.count == 0 {
+            return true
+        }
+        var properties = [String: String]()
+        let firstLine = lines[0]
+        let titleLineRegex = try? NSRegularExpression(pattern: "^= (.*)$")
+        let range = NSRange(location: 0, length: firstLine.count)
+        let titleLineMatch = titleLineRegex!.matches(
+            in: firstLine, options: [], range: range
+        )
+        if titleLineMatch.count > 0 {
+            let title = titleLineRegex!.stringByReplacingMatches(
+                in: firstLine, options: [], range: range, withTemplate: "$1"
+            )
+            if title.count > 0 {
+                properties["doctitle"] = title
+            }
+        }
+        let fieldLineRegex = try? NSRegularExpression(pattern: "^:(\\w+): (.*)")
+        for i in 1..<lines.count {
+            let line = lines[i]
+            let allOfLine = NSRange(location: 0,
+                                    length: line.count)
+            let match = fieldLineRegex!.matches(in: line, options: [],
+                                                range: allOfLine)
+            if match.count > 0 {
+                let key = fieldLineRegex!.stringByReplacingMatches(
+                    in: line, options: [], range: allOfLine,
+                    withTemplate: "$1"
+                )
+                let value = fieldLineRegex!.stringByReplacingMatches(
+                    in: line, options: [], range: allOfLine,
+                    withTemplate: "$2"
+                )
+                properties[key] = value
+            } else {
+                break
+            }
+        }
+        NSLog("AsciiDoc file attributes: \(properties)")
+        if let title = properties["doctitle"] {
+            attributes[kMDItemTitle!] = title
+        }
+        if let author = properties["author"] {
+            attributes[kMDItemAuthors!] = [author]
+        }
+        if let created = properties["created"] {
+            attributes[kMDItemContentCreationDate!] = created
+        }
+        if let revdate = properties["revdate"] {
+            attributes[kMDItemContentModificationDate!] = revdate
+        }
+        var keywords: [String] = []
+        if let keyword_prop = properties["keywords"] {
+            let terms = keyword_prop.components(separatedBy: ",")
+            for term in terms {
+                let keyword = term.trimmingCharacters(in: .whitespaces)
+                keywords.append(keyword)
+            }
+        }
+        if let category_prop = properties["categories"] {
+            let terms = category_prop.components(separatedBy: ",")
+            for term in terms {
+                let category = "_" + term.trimmingCharacters(in: .whitespaces)
+                keywords.append(category)
+            }
+        }
+        if keywords.count > 0 {
+            attributes[kMDItemKeywords!] = keywords
+        }
+        return true
     }
 }
