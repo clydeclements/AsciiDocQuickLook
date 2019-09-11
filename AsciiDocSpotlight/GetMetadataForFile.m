0778f1d3a37fce4f1732472cdd338e4d84fde697
Clyde Clements <clyde.clements@gmail.com>
2019-09-11 10:16:54 -0230

    Attempt to use XPC service in Spotlight plugin - Does not work!
    Use of XPC service denied due to sandbox restrictions.

--- AsciiDocSpotlight/GetMetadataForFile.m
+++ AsciiDocSpotlight/GetMetadataForFile.m
@@ -8,7 +8,8 @@
 
 #include <CoreFoundation/CoreFoundation.h>
 #import <CoreData/CoreData.h>
-#import "AsciiDoc.h"
+#import "AsciiDocHelperProtocol.h"
+//#import "AsciiDoc.h"
 
 Boolean GetMetadataForFile(void *thisInterface,
                            CFMutableDictionaryRef attributes,
@@ -38,8 +39,36 @@ Boolean GetMetadataForFile(void *thisInterface,
     @autoreleasepool {
         NSLog(@"AsciiDoc Spotlight Plug-in: Attempting to import file %@", (__bridge NSString *)pathToFile);
         if ([(__bridge NSString *)contentTypeUTI isEqualToString:@"org.asciidoc"]) {
+            /*
             AsciiDoc *adoc = [[AsciiDoc alloc] initWithPath:pathToFile];
             [adoc getMetadata:(__bridge NSMutableDictionary *)attributes];
+            */
+            //NSMutableDictionary *attrs = (__bridge NSMutableDictionary *)attributes;
+            NSXPCConnection *helperConnection = [[NSXPCConnection alloc]
+                                                 initWithMachServiceName:@"ca.bluemist2.AsciiDocHelper"
+                                                 options:0];
+            helperConnection.remoteObjectInterface = [NSXPCInterface interfaceWithProtocol:@protocol(AsciiDocHelperProtocol)];
+            // TODO: Set interruption handler.
+            //helperConnection.interruptionHandler = <#^(void)#>
+            // TODO: Set invalidation handler?
+            //helperConnection.invalidationHandler = <#^(void)#>
+            [helperConnection resume];
+            NSLog(@"Connection to AsciiDocHelper established");
+            
+            id remoteObject = [helperConnection remoteObjectProxyWithErrorHandler:^(NSError *error) {
+                NSLog(@"Error in AsciiDocHelper connection: %@", [error debugDescription]);
+            }];
+            [remoteObject getMetadata:(__bridge NSString *)pathToFile withReply:^(NSData *data) {
+                // We have received a response. Update our text field, but do it on the main thread.
+                NSLog(@"Reply received");
+                NSDictionary *metadata = (NSDictionary *)[NSKeyedUnarchiver unarchiveObjectWithData:data];
+                NSLog(@"Metadata: %@", metadata);
+            }];
+            
+            NSLog(@"Waiting for response from AsciiDocHelper");
+            [NSThread sleepForTimeInterval:1.0];
+            NSLog(@"Closing connection to AsciiDocHelper");
+            [helperConnection invalidate];
         }
     }
 
